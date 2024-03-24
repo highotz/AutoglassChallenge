f85932f3369b9e642d80d37fae24b3e14ee7bd25
Higor Hotz Vasconcelos <highotz.hotz@hotmail.com>
2024-03-24 20:31:15 -0300

    Add Product

--- AutoglassChallenge.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AutoglassChallenge.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ AutoglassChallenge.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoglassChallenge.Domain.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public bool Status { get; set; } = true;
+        public DateTime manufacturingDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int SupplierCode { get; set; }
+        public string? SupplierDescription { get; set; }
+        public string? SupplierCNPJ { get; set; }
+    }
+}
