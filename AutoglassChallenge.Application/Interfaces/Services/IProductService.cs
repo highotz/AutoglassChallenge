a89dea6ddb9f3266ee47c7d4cd4b796ac0357786
Higor Hotz Vasconcelos <highotz.hotz@hotmail.com>
2024-03-25 14:39:34 -0300

    commit

---
+++ AutoglassChallenge.Application/Interfaces/Services/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoglassChallenge.Application.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Task<bool> Add(ProductRequest productRequest);
+        IAsyncEnumerable<ProductResponse> GetAll();
+        Task<ProductResponse> GetId(int productId);
+        Task<bool> Update(int productId, ProductRequest request);
+        Task<ProductResponse> Delete(int productId);
+
+    }
+}
--- AutoglassChallenge.Domain/Entities/Product.cs
+++ AutoglassChallenge.Domain/Entities/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AutoglassChallenge.Domain.Entities
 {
-    internal class Product
+    public class Product
     {
         public int Id { get; set; }
         public string? Description { get; set; }
