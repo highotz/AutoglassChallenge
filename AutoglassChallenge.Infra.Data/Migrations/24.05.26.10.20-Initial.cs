bf000123151f9fd738e6bb2742afaf2dd8323bde
Higor Hotz Vasconcelos <highotz.hotz@hotmail.com>
2024-03-26 10:36:01 -0300

    add Migration

---
+++ AutoglassChallenge.Infra.Data/Migrations/24.05.26.10.20-Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Data.Migrations
+{
+    public partial class M2403261020Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "NVARCHAR(250)", maxLength: 250, nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Manufacturing = table.Column<DateTime>(type: "datetime2", nullable: true, defaultValueSql: "GETDATE()"),
+                    Validate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    SupplierCode = table.Column<string>(type: "int", nullable: false),
+                    SupplierDescription = table.Column<string>(type: "NVARCHAR(200)", maxLength: 200, nullable: false),
+                    SupplierCNPJ = table.Column<string>(type: "VARCHAR(14)", maxLength: 14, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Id", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
